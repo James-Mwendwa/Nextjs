fd8ef607253c25d7c2b85570e2772943f38408ae
James Mwendwa <mwendwajames32@gmail.com>
2022-09-17 21:00:50 +0300

    nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h2>Next JS</h2>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h2 className={styles.title}>Homepage</h2>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          quaerat ducimus soluta ea excepturi. Nisi molestias nostrum labore,
+          eligendi reiciendis tempore ut accusantium blanditiis laborum, cumque
+          saepe. reiciendis tempore ut accusantium blanditiis laborum, cumque
+          saepe
+        </p>
+
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
+  );
 }
