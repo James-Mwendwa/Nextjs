735765986e28690753833b4c65d4a56608243561
James Mwendwa <mwendwajames32@gmail.com>
2022-10-04 13:08:23 +0300

    nextjs

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,18 @@
-import Head from 'next/Head'
-import Link from 'next/link';
-import Script from 'next/script';
-
+import Head from "next/Head";
+import Link from "next/link";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
-    <Head>
-      <title>First Post</title>
-    </Head>
-     
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
